83214a17b7ea3c28a207a0d500d597687239030c
manhloi5501 <manhloi0505@gmail.com>
2021-10-07 22:48:03 +0700

    add video to app

--- src/screens/WatchMovieScreen/WatchMovieScreen.tsx
+++ src/screens/WatchMovieScreen/WatchMovieScreen.tsx
@@ -1,7 +1,7 @@
 import {useRoute} from '@react-navigation/core';
 import React from 'react';
 import {View, Text} from 'react-native';
-// import Video from 'react-native-video';
+import Video from 'react-native-video';
 
 const WatchMovieScreen = () => {
   const route = useRoute();
@@ -10,16 +10,13 @@ const WatchMovieScreen = () => {
 
   return (
     <View style={{flex: 1}}>
-      {/* <Video
-        source={{uri: 'https://www.w3schools.com/html/mov_bbb.mp4'}}
-        style={{
-          position: 'absolute',
-          top: 0,
-          left: 0,
-          bottom: 0,
-          right: 0,
+      <Video
+        controls
+        source={{
+          uri: 'https://www.w3schools.com/html/mov_bbb.mp4',
         }}
-      /> */}
+        style={{flex: 1}}
+      />
     </View>
   );
 };
